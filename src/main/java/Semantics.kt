2637e79accb2a972ca97d8181bcf933c0e7c87a1
ksqsf <i@ksqsf.moe>
2021-02-25 15:13:01 +0800

    Semantics: refactor, and add rollback

--- src/main/java/Semantics.kt
+++ src/main/java/Semantics.kt
@@ -8,10 +8,10 @@ val basicUpdates = setOf(
     "java.sql.PreparedStatement.executeUpdate"
 )
 
-fun containsUpdate(s: Expression): Boolean {
+fun containsCall(nameSet: Set<QualifiedName>, s: Expression): Boolean {
     return s.accept(object : GenericVisitorAdapter<Boolean, Void?>() {
         override fun visit(m: MethodCallExpr, arg_: Void?): Boolean? {
-            if (basicUpdates.contains(m.resolve().qualifiedName)) {
+            if (nameSet.contains(m.resolve().qualifiedName)) {
                 return true
             }
             return null
@@ -19,12 +19,46 @@ fun containsUpdate(s: Expression): Boolean {
     }, null) ?: return false
 }
 
-fun containsUpdate(s: Statement): Boolean {
+fun containsCall(nameSet: Set<QualifiedName>, s: Statement): Boolean {
     return s.accept(object : GenericVisitorAdapter<Boolean, Void?>() {
         override fun visit(m: MethodCallExpr, arg_: Void?): Boolean? {
-            if (containsUpdate(m))
+            if (containsCall(nameSet, m))
                 return true
             return null
         }
     }, null) ?: return false
+}
+
+fun containsUpdate(s: Expression): Boolean {
+    return containsCall(basicUpdates, s)
+}
+
+fun containsUpdate(s: Statement): Boolean {
+    return containsCall(basicUpdates, s)
+}
+
+val basicCommits = setOf(
+    "java.sql.Connection.commit",
+    "edu.rice.rubis.servlets.Database.commit", // FIXME: we haven't implemented precise analysis yet
+)
+
+fun containsCommit(s: Expression): Boolean {
+    return containsCall(basicCommits, s)
+}
+
+fun containsCommit(s: Statement): Boolean {
+    return containsCall(basicCommits, s)
+}
+
+val basicRollback = setOf(
+    "java.sql.Connection.rollback",
+    "edu.rice.rubis.servlets.Database.rollback"
+)
+
+fun containsRollback(s: Expression): Boolean {
+    return containsCall(basicRollback, s)
+}
+
+fun containsRollback(s: Statement): Boolean {
+    return containsCall(basicRollback, s)
 }
