a8d6df258260a1fa4f2aebaf0f73a2f5568311ff
ksqsf <i@ksqsf.moe>
2021-02-25 22:55:19 +0800

    Semantics: (unfinished) add semantics of a few essential JDBC primitives

--- src/main/java/Semantics.kt
+++ src/main/java/Semantics.kt
@@ -2,6 +2,7 @@ import com.github.javaparser.ast.expr.Expression
 import com.github.javaparser.ast.expr.MethodCallExpr
 import com.github.javaparser.ast.stmt.Statement
 import com.github.javaparser.ast.visitor.GenericVisitorAdapter
+import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration
 
 val basicUpdates = setOf(
     "java.sql.Statement.executeUpdate",
@@ -61,4 +62,61 @@ fun containsRollback(s: Expression): Boolean {
 
 fun containsRollback(s: Statement): Boolean {
     return containsCall(basicRollback, s)
+}
+
+val knownSemantics = mapOf(
+    "java.sql.Connection.prepareStatement" to ::prepareStatementSemantics,
+    "java.sql.PreparedStatement.executeQuery" to ::executeQuerySemantics,
+    "java.sql.PreparedStatement.setInt" to ::setParameterSemantics,
+    "java.sql.PreparedStatement.setLong" to ::setParameterSemantics,
+    "java.sql.PreparedStatement.setDate" to ::setParameterSemantics,
+    "java.sql.PreparedStatement.setDouble" to ::setParameterSemantics,
+    "java.sql.PreparedStatement.setFloat" to ::setParameterSemantics,
+    "java.sql.ResultSet.getInt" to ::getColumnSemantics,
+    "java.sql.ResultSet.getLong" to ::getColumnSemantics,
+    "java.sql.ResultSet.getDate" to ::getColumnSemantics,
+    "java.sql.ResultSet.getDouble" to ::getColumnSemantics,
+    "java.sql.ResultSet.getFloat" to ::getColumnSemantics,
+)
+
+fun hasSemantics(methodDecl: ResolvedMethodDeclaration): Boolean {
+    return knownSemantics.containsKey(methodDecl.qualifiedName)
+}
+
+fun dispatchSemantics(self: Expression, env: Interpreter, receiver: AbstractValue?, args: List<AbstractValue>): AbstractValue {
+    return knownSemantics[self.asMethodCallExpr().resolve().qualifiedName]!!(self, env, receiver, args)
+}
+
+fun prepareStatementSemantics(self: Expression, env: Interpreter, receiver: AbstractValue?, args: List<AbstractValue>): AbstractValue {
+    // Receiver is a Connection object, ignore it.
+    // The first argument is the SQL string.
+    val sqlStrVal = args[0]
+
+    val sqlStr = if (sqlStrVal is AbstractValue.Data && sqlStrVal.data is String) {
+        sqlStrVal.data
+    } else {
+        sqlStrVal.guessSql()
+    }
+
+    return AbstractValue.SqlStmt(self, self.calculateResolvedType(), sqlStr)
+}
+
+fun executeQuerySemantics(self: Expression, env: Interpreter, receiver: AbstractValue?, args: List<AbstractValue>): AbstractValue {
+    // Receiver is a prepared statement.
+    assert(receiver is AbstractValue.SqlStmt)
+    val receiver = receiver as AbstractValue.SqlStmt
+    val sqlStr = receiver.sql
+    TODO()
+}
+
+fun setParameterSemantics(self: Expression, env: Interpreter, receiver: AbstractValue?, args: List<AbstractValue>): AbstractValue {
+    val receiver = receiver!! as AbstractValue.SqlStmt
+    val idx = (args[0] as AbstractValue.Data).data as Long
+    val param = args[1]
+    receiver.setParameter(idx.toInt(), param)
+    return AbstractValue.Unknown(self, self.calculateResolvedType())
+}
+
+fun getColumnSemantics(self: Expression, env: Interpreter, receiver: AbstractValue?, args: List<AbstractValue>): AbstractValue {
+    TODO()
 }
