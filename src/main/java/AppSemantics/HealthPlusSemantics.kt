c65bcf59d3b9c34173eb01b70d93bc48edd9d7e0
ksqsf <i@ksqsf.moe>
2021-04-04 14:31:25 +0800

    HealthPlus: collect approx SQL

--- src/main/java/AppSemantics/HealthPlusSemantics.kt
+++ src/main/java/AppSemantics/HealthPlusSemantics.kt
@@ -4,14 +4,91 @@ import AbstractValue
 import com.github.javaparser.ast.expr.Expression
 import knownSemantics
 import Interpreter
+import java.lang.IllegalArgumentException
 
-// This file deals with original HealthPlus code.
+// This file deals with the original HealthPlus code, not the synthetic one.
+//
+// There are some subtleties:
+//
+// HealthPlus builds SQL queries by constructing query strings, not a good practice, but anyway we stick to the
+// original semantics.
+//     "UPDATE sys_user SET " + column_data + " WHERE user_id=" +
+//     "(SELECT user_id FROM doctor WHERE slmc_reg_no='" + this.slmcRegNo + "');"
+// The pluses totally mess up the AST structure, and column_data is entirely opaque.
+//
+// 1. We adopt a conservative analysis strategy, i.e. we think column_data of
+//        field1 = argv[field1], field2 = argv[field2], ...
+//    Since argv are opaque to Z3, this is safe.
+//
+// 2. To parse it, we resort to a hand-made one. We collected all approximate SQL string templates, and the complex patterns are
+//    1) INSERT INTO medical_history VALUES ('[[v7]]','[[patientID8]]','[[v9|this.slmcRegNo]]','[[v10]]','[[diagnostic11]]')
+//    2) UPDATE person SET [...] WHERE person_id = (SELECT person_id FROM sys_user WHERE user_id = (SELECT user_id FROM doctor WHERE slmc_reg_no = '[...]'))
+//    3) INSERT INTO tmp_bill ([...]) VALUES ([...])
+//    4) SELECT stock_id FROM pharmacy_stock WHERE stock_id = (SELECT MAX(stock_id) FROM pharmacy_stock)
+//
+// Anyway, this is a pretty ad hoc strategy, and the relevant code is only put here.
 
 fun registerHealthPlusSemantics() {
     knownSemantics["com.hms.hms_test_2.DatabaseOperator.customInsertion"] = ::customInsertionSemantics
+    knownSemantics["com.hms.hms_test_2.DatabaseOperator.customSelection"] = ::customSelectionSemantics
 }
 
+// NOTE: Some UPDATE queries are actually handled by customInsertion.
 fun customInsertionSemantics(self: Expression, env: Interpreter, receiver: AbstractValue?, args: List<AbstractValue>): AbstractValue {
-    println("Found expression $self")
+    val approxSql = approximateSQL(args[0])
+    println("Insertion Approx = $approxSql")
     return AbstractValue.Unknown(self, self.calculateResolvedType())
+}
+
+fun customSelectionSemantics(self: Expression, env: Interpreter, receiver: AbstractValue?, args: List<AbstractValue>): AbstractValue {
+    val approxSql = approximateSQL(args[0])
+    println("Selection Approx = $approxSql")
+    return AbstractValue.Unknown(self, self.calculateResolvedType())
+}
+
+var cnt = 0
+
+/**
+ * Return an approximation of the SQL string building expression, for further parsing.
+ */
+fun approximateSQL(av: AbstractValue): String {
+    when (av) {
+        is AbstractValue.Binary -> {
+            when (av.op) {
+                Operator.ADD -> {
+                    return approximateSQL(av.left) + approximateSQL(av.right)
+                }
+                else -> {
+                    throw IllegalArgumentException("Unknown binary operator ${av.op}")
+                }
+            }
+        }
+        is AbstractValue.Data -> {
+            return when (av.data) {
+                is String -> av.data
+                is Long -> av.data.toString()
+                is Double -> av.data.toString()
+                else -> throw IllegalArgumentException("Unknown data type ${av.data::class}")
+            }
+        }
+        is AbstractValue.Unknown -> {
+            cnt += 1
+            return "[[v$cnt|${av.e}]]"
+        }
+        is AbstractValue.Call -> {
+            cnt += 1
+            return "[[v$cnt]]"
+        }
+        is AbstractValue.Free -> {
+            cnt += 1
+            return "[[${av.e}$cnt]]"
+        }
+        is AbstractValue.Null -> {
+            cnt += 1
+            return "[[${av.e}$cnt]]"
+        }
+        else -> {
+            throw IllegalArgumentException("Unknown $av")
+        }
+    }
 }
